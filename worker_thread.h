fddddbf3d461ae3ef263c937182a8cda85c6181d
Oleg Kutkov <elenbert@gmail.com>
2017-05-12 03:17:51 +0300

    temporaly remove string filed of the coords struct

--- worker_thread.h
+++ worker_thread.h
@@ -86,7 +86,7 @@ struct POSITION_REQUEST
 	double pos_ra_speed;
 	double pos_dec_speed;
 	bool pos_failed;
-	std::string error_text;
+//	std::string error_text;
 };
 
 class WorkerThread : public wxThread
