a7c5f7867a861f6da6b8bf754ea194c1afaa86a7
Oleg Kutkov <oleg@oleg-lab.craocrimea.ru>
2017-06-06 01:28:18 +0300

    new icons for scope control buttons

---
+++ icons/east_running.h
@@ -0,0 +1,12 @@
+#ifndef _EAST_RUNNING_H_
+#define _EAST_RUNNING_H_
+
+static const unsigned char east_running_png[] = {
+"\x89PNG\15\12\32\12\0\0\0\15IHDR\0\0\0\27\0\0\0\26\10\6\0\0\0+v\7\5\0\0\0\6""bKGD\0\xFF\0\xFF\0"
+"\xFF\xA0\xBD\xA7\x93\0\0\0\tpHYs\0\0\13\23\0\0\13\23\1\0\x9A\x9C\30\0\0\0\7tIME\7\xE1\6\5\25.\17("
+"\7\22""3\0\0\0\31tEXtComment\0""Created with GIMPW\x81\16\27\0\0\0""3IDAT8\xCB""c`\30\5\xC3\12""0"
+"\xE2\x95\xBDn\xFB\37\xAF\xBC\xE6""a\xBC\xFA\x99h\xE9r\26\xA2T\21p\xE1\x80\xB8\x9Ci4BG#t4\xB5\x8C"
+"\x82\xA1\6\0M\xA5\12\xF6""a\37\xD7\xCB\0\0\0\0IEND\xAE""B`\x82"
+};
+
+#endif
---
+++ icons/east_stopped.h
@@ -0,0 +1,12 @@
+#ifndef _EAST_STOPPED_H_
+#define _EAST_STOPPED_H_
+
+static const char east_stopped_png[] = {
+"\x89PNG\15\12\32\12\0\0\0\15IHDR\0\0\0\27\0\0\0\26\10\6\0\0\0+v\7\5\0\0\0\6""bKGD\0\xFF\0\xFF\0"
+"\xFF\xA0\xBD\xA7\x93\0\0\0\tpHYs\0\0\13\23\0\0\13\23\1\0\x9A\x9C\30\0\0\0\7tIME\7\xE1\6\5\15%9\26|"
+"\xAC\xA9\0\0\0\31tEXtComment\0""Created with GIMPW\x81\16\27\0\0\0+IDAT8\xCB""c`\30\5\xC3\12""0\22"
+"\x90\xFFO\x89~&Z\xBA\x9C\x85J>\xA4\xBF\xCB\x99""F#t4BGS\xCB(\30j\0\0\xD5p\3\36""6\16\xB0\\\0\0\0\0"
+"IEND\xAE""B`\x82"
+};
+
+#endif
